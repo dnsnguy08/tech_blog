b261cf4d55268ed890c69f467597ef1243d25a53
Dennis Nguyen <dnsnguy08@gmail.com>
2022-08-02 17:29:01 -0700

    created connection to database and added seed info to insert into db

---
+++ seeds/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        username: 'dnsnguy08',
+        password: 'passpass',
+    },
+    {
+        username: 'coolio',
+        password: 'yoyoyoo',
+    },
+    {
+        username: 'helloworld',
+        password: 'gotitdude',
+    },
+];
