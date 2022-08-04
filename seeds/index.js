f649e7b62522c2d6f6fde86de3388489c8f292d4
Dennis Nguyen <dnsnguy08@gmail.com>
2022-08-03 18:54:54 -0700

    added view layouts for the app

--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,7 @@ const users = require('./users');
 const {User} = require('../models');
 const sequelize = require('../config/connection');
 
+// Create db data by running seeder
 const seeder = async () => {
     await sequelize.sync({force: true});
     await User.bulkCreate(users, {
