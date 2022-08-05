4e3460f87958f6a5b05211350168e9b348330492
Dennis Nguyen <dnsnguy08@gmail.com>
2022-08-04 17:14:48 -0700

    refactored server.js and fixed declaration stack

--- server.js
+++ server.js
@@ -1,62 +1,47 @@
-require('dotenv').config();
 const express = require('express');
 const exphbs = require('express-handlebars');
-const expsesh = require('express-session');
-
-const SequelizeStore = require('connect-session-sequelize')(expsesh.Store);
-const sequelize = require('./config/connection');
-
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
 
 // handlebars helpers
 const helpers = require('./utils/helpers');
 
-// const exphbs = require('express-handlebars');
-
 // handlebars init
-const hbs = exphbs.create({
-    helpers,
-});
-
-// const session = require('express-session');
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const hbs = exphbs.create({ helpers });
 
 // express session settings
-const sessionSettings = {
+const sess = {
     secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
+    cookie: {
+        // Session will automatically expire in 10 minutes
+        expires: 10 * 60 * 1000
+    },
+    resave: true,
+    rolling: true,
+    saveUninitialized: true,
     store: new SequelizeStore({
-        db: sequelize,
+        db: sequelize
     }),
 };
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// handlebars template engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.use(express.static('public'));
 
-// app.use(session(sessionSettings));
-app.use(expsesh(sessionSettings));
-
-// app.use(express.static('public'));
-
+// middlewares
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static('public'));
-
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Success, Server Up!'));
+    app.listen(PORT, () => console.log('Now listening'));
 });
