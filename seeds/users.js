2a58d542f22e4c84bb5d020857368b587b3ed945
Dennis Nguyen <dnsnguy08@gmail.com>
2022-08-04 17:13:24 -0700

    created test seeds for database and added helper function for formatting plural html elements

--- seeds/users.js
+++ seeds/users.js
@@ -1,14 +1,17 @@
 module.exports = [
     {
         username: 'dnsnguy08',
+        email: 'dnsnguy08@gmail.com',
         password: 'passpass',
     },
     {
         username: 'coolio',
+        email: 'coolio@gmail.com',
         password: 'yoyoyoo',
     },
     {
         username: 'helloworld',
+        email: 'helloworld@gmail.com',
         password: 'gotitdude',
     },
 ];
